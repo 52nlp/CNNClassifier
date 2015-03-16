b2d988a37030847b2b445d3ea561a8be57a0ac8f
liuchen11 <liuchen112013@gmail.com>
2015-03-16 14:31:46 +0800

    main function

---
+++ main.py
@@ -0,0 +1,55 @@
+import sys
+import theano
+import numpy as np
+
+from cnn import *
+from libparser import *
+
+# theano.config.exception_verbosity='high'
+
+if __name__=='__main__':
+	if len(sys.argv)!=2:
+		print 'Usage: python main.py <dataset>(cifar-10 or svhm)'
+		exit(0)
+	else:
+		path=sys.argv[1]+'/'
+
+	train=unzip(path+'train_data')
+	validate=unzip(path+'validate_data')
+	test=unzip(path+'test_data')
+
+	train_data=np.asarray(train['data'],dtype=theano.config.floatX)
+	train_label=np.asarray(train['labels'],dtype=theano.config.floatX)
+	validate_data=np.asarray(validate['data'],dtype=theano.config.floatX)
+	validate_label=np.asarray(validate['labels'],dtype=theano.config.floatX)
+	test_data=np.asarray(test['data'],dtype=theano.config.floatX)
+	test_label=np.asarray(test['labels'],dtype=theano.config.floatX)
+
+	machine=model(
+		learn_rate=0.005,
+		n_epochs=100,
+		filters=[128,128,128,256,256],
+		batch_size=25
+		)
+
+	train_set_data=np.asarray(train_data,dtype=theano.config.floatX)
+	train_set_label=np.asarray(train_label,dtype=int)
+	validate_set_data=np.asarray(validate_data,dtype=theano.config.floatX)
+	validate_set_label=np.asarray(validate_label,dtype=int)
+	test_set_data=np.asarray(test_data,dtype=theano.config.floatX)
+	test_set_label=np.asarray(test_label,dtype=int)
+
+	for i in xrange(len(train_set_data)):
+		train_set_data[i]-=train_set_data[i].mean()
+
+	for i in xrange(len(validate_set_data)):
+		validate_set_data[i]-=validate_set_data[i].mean()
+
+	for i in xrange(len(test_set_data)):
+		test_set_data[i]-=test_set_data[i].mean()
+
+	machine.train_validate_test(
+		train_set_data,train_set_label,
+		validate_set_data,validate_set_label,
+		test_set_data,test_set_label
+		)
